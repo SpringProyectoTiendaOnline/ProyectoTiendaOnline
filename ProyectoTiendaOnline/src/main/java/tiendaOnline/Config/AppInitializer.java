3a09ba1676091d2be06062c2c176971f317cd95e
six <yangmi1998@gmail.com>
2020-04-14 15:42:57 +0200

    commit

--- ProyectoTiendaOnline/src/main/java/tiendaOnline/Config/AppInitializer.java
+++ ProyectoTiendaOnline/src/main/java/tiendaOnline/Config/AppInitializer.java
@@ -3,7 +3,7 @@ package tiendaOnline.Config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
+	//Inicializar
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class[] {};
