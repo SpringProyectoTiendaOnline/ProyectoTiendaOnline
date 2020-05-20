676bef607c31e4a0fb76955e5bd60e0e5677116b
irene <irene.avellas@hotmail.com>
2020-05-20 12:44:45 +0200

    Internacionalizacion antes del login con parte de validacion

--- ProyectoTiendaOnline/src/main/java/tiendaOnline/Config/WebMvcConfig.java
+++ ProyectoTiendaOnline/src/main/java/tiendaOnline/Config/WebMvcConfig.java
@@ -4,16 +4,22 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
@@ -21,7 +27,7 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "tiendaOnline" })
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer  {
 
 	@Autowired
 	private ApplicationContext applicationContext;
@@ -120,13 +126,44 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler(
 				"/css/**",
 				"/js/**",
-				"/imagen/**"
+				"/imagen/**",
+				"/i18n/**"
 				).addResourceLocations(
 						"/WEB-INF/css/",
 						"/WEB-INF/js/",
-						"/WEB-INF/imagen/")
+						"/WEB-INF/imagen/",
+						"/WEB-INF/i18n/")
 				.setCachePeriod(3600);
 	}
 	
 	
+	
+	     
+	 @Bean(name = "localeResolver")
+	    public LocaleResolver getLocaleResolver()  {
+	        CookieLocaleResolver resolver= new CookieLocaleResolver();
+	        resolver.setCookieDomain("myAppLocaleCookie");
+	        // 60 minutes 
+	        resolver.setCookieMaxAge(60*60); 
+	        return resolver;
+	    } 
+	     
+	    @Bean(name = "messageSource")
+	    public MessageSource getMessageResource()  {
+	        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+	         
+	        // Read i18n/messages_xxx.properties file.
+	        // For example: i18n/messages_en.properties
+	        messageResource.setBasename("classpath:i18n/messages");
+	        messageResource.setDefaultEncoding("UTF-8");
+	        return messageResource;
+	    }
+	    @Override
+	    public void addInterceptors(InterceptorRegistry registry) {
+	        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+	        localeInterceptor.setParamName("lang");
+	         
+	         
+	        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+	    }
 }
